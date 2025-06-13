9dc90be80428ca1d38ec00ed68e49879fa97d0f3
HJZR2004 <A01027743@tec.mx>
2025-06-12 22:36:16 -0600

    the test works good by using the env file

--- cypress/e2e/RolRequestApplication.cy.ts
+++ cypress/e2e/RolRequestApplication.cy.ts
@@ -1,28 +1,12 @@
-describe('Verificación de viaje con destino Bolivia para dos usuarios', () => {
-    it('test y test5 deben ver solicitudes con destino Bolivia', () => {
-      // --- Login como "test"
-      cy.visit('https://localhost:4321/login');
-      cy.get('input#username').type('test');
-      cy.get('input#password').type('test');
-      cy.get('form').submit();
-  
-      cy.url().should('include', '/dashboard');
-  
-      // Verifica "País Destino: Bolivia"
-      cy.contains('p', 'País Destino: Bolivia').should('exist');
-  
-      // --- Simula logout visitando manualmente /login
-      cy.visit('https://localhost:4321/login');
-  
-      // --- Login como "test5"
-      cy.get('input#username').clear().type('test4');
-      cy.get('input#password').clear().type('test');
-      cy.get('form').submit();
-  
-      cy.url().should('include', '/dashboard');
-    
-      // Verifica que aparece el texto "Viaje Rumbo a Bolivia"
-      cy.contains('Viaje Rumbo a Bolivia').should('exist');
-    });
+describe('Login como solicitante y verifica Bolivia', () => {
+  it('Inicia sesión y verifica viaje con destino Bolivia', () => {
+    const user = Cypress.env('SOLICITANTE_USER');
+    const password = Cypress.env('SOLICITANTE_PASSWORD');
+
+    cy.visit('/login');
+    cy.get('input#username').type(user);
+    cy.get('input#password').type(password);
+    cy.get('form').submit();
+
   });
-  
+});
